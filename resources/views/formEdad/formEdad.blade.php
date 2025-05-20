e3c5e7a35f0f4bd179fee489a22d2e3e1d3d236f
OppaDev <jlobando2@espe.edu.ec>
2025-05-19 20:40:55 -0500

    form de edad

---
+++ resources/views/formEdad/formEdad.blade.php
@@ -0,0 +1,12 @@
+
+<div class="contenedor-form">
+    <h1>Formulario Edad</h1>
+    <form action="" method="">
+        @csrf
+        <div class="form-group">
+            <label for="edad">Edad:</label>
+            <input type="number" id="edad" name="edad" required>
+        </div>
+        <button type="submit">Enviar</button>
+    </form>
+</div>
